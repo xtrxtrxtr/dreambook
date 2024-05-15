f59c0291e5d2866e1f3d826769679bad82dd41a0
Maxim Khan-Magomedov <maxim.km@gmail.com>
2024-05-15 18:55:08 +0300

    Update bundle

--- spec/models/pattern_spec.rb
+++ spec/models/pattern_spec.rb
@@ -9,9 +9,11 @@ RSpec.describe Pattern do
 
   it_behaves_like 'has_uuid'
 
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(50) }
-  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
-  it { is_expected.to validate_length_of(:summary).is_at_least(1).is_at_most(500).allow_blank }
-  it { is_expected.to validate_presence_of(:description) }
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(50) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+    it { is_expected.to validate_length_of(:summary).is_at_least(1).is_at_most(500).allow_blank }
+    it { is_expected.to validate_presence_of(:description) }
+  end
 end
